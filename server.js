2564843f38d29876748d4bbfedea41e24c2725cf
ARodriguezJr <31947104+ARodriguezJr@users.noreply.github.com>
2019-11-25 11:29:23 -0800

    Able to receive orders from listings

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var https = require('https')
 var path = require('path')
 var router = express.Router();
 var cors = require('cors')
+var url = require('url')
+var bodyParser = require('body-parser')   // Maybe not needed
+var querystring = require('querystring')  // Maybe not needed
 let app = express()
 
 app.use(cors())
 
-var post = require('./post.js')
+//var post = require('./post.js')
 
 /*app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,13 +19,57 @@ var post = require('./post.js')
   next();
  }); */
 
+
+
 const pug = require('pug');
 
 app.use("/js", express.static(__dirname + "/js"));
 app.use("/node_modules", express.static(__dirname + "/js"));
 app.use("/static", express.static(__dirname + "/static"));
+app.use("/orders", express.static(__dirname + "/orders"));
 
 app.set("view engine", "pug");
+
+app.use(bodyParser.urlencoded({    
+  extended: true
+})); 
+
+app.post('/incomingOrder', function(req, res) {
+  var item = req.body.item;
+  var quantity = req.body.quantity;
+  fs.writeFile("orders/adminorders.txt", item + " #" + quantity, (err) => {
+    if(err) throw err;
+
+    console.log("Order saved!");
+  });
+
+  res.send(item + ' ' + quantity);
+}); 
+
+/*app.get('/incomingOrder', function(req, res) {
+  var item = req.param('item');
+  var quantity = req.param('quantity');
+
+  res.send(item + ' ' + quantity);
+}); */
+
+// Function to handle the root path
+/*app.get('/', (req, res) => {
+  console.log(req.query)
+}) */
+
+/*app.use(function(request, response){
+  var queryData = url.parse(request.url, true).query;
+  response.writeHead(200, {"Content-Type": "text/plain"});
+
+  if (queryData.name) {
+    // user told us their name in the GET request, ex: http://host:8000/?name=Tom
+    response.end('Hello ' + queryData.name + '\n');
+
+  } else {
+    response.end("Hello World\n");
+  }
+}); */
 
 //app.set("views", path.join(__dirname, "views"));
 
